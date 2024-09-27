bde239623132d91cd91698d35287cf086ca361b5
Princess-Morris <okpataprincessmorris3@gmail.com>
2024-09-27 11:02:13 +0100

    feat: create a comming soon page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Oduko Ositadimma profile Website
+    // <div>
+    //   Oduko Ositadimma profile Website
+    // </div>
+    <div className="coming-soon-container">
+      <h1>Coming Soon</h1>
+      <p>We are working hard to launch our new website.</p>
+      <p>Stay tuned for something amazing!</p>
     </div>
   );
 }
