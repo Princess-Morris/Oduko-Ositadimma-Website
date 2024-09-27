0bbe4711b0cf95419afe6926772c30e621c8693c
Princess-Morris <okpataprincessmorris3@gmail.com>
2024-09-27 11:09:10 +0100

    style: changed the profile styling to comming soon

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ function App() {
     //   Oduko Ositadimma profile Website
     // </div>
     <div className="coming-soon-container">
-      <h1>Coming Soon</h1>
-      <p>We are working hard to launch our new website.</p>
+      <h1>Welcome, and thanks for checking me out</h1>
+      <h1>Oduko Ositadimma</h1>
+
+      <p>Hi! my name is Mazi Ositadimma Oduko. Noticed you are trying to check my profile out, well me and my developers are making some changes and would be back soon.</p>
       <p>Stay tuned for something amazing!</p>
     </div>
   );
